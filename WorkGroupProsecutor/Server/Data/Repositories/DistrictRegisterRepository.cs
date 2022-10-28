c3d122b56a0d25170b79843deda23b2076e87efd
Rh Proj <r.proj@hotmail.com>
2022-10-28 16:40:54 +0300

    .

---
+++ WorkGroupProsecutor/Server/Data/Repositories/DistrictRegisterRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using WorkGroupProsecutor.Server.Data.Context;
+using WorkGroupProsecutor.Shared.Models;
+
+namespace WorkGroupProsecutor.Server.Data.Repositories
+{
+    public class DistrictRegisterRepository : IDistrictRegisterRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public DistrictRegisterRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IList<DistrictRegisterModel>> GetAllDistrictRegistry()
+        {
+            return await _dbContext.DistrictRegister.ToListAsync();
+        }
+
+        public async Task<DistrictRegisterModel> GetDistrictRegisterById(int id)
+        {
+            return await _dbContext.DistrictRegister.FirstOrDefaultAsync(r => r.Id == id);
+        }
+
+        public async Task AddDistrictRegister(DistrictRegisterModel register)
+        {
+            ArgumentNullException.ThrowIfNull(register);
+            await _dbContext.DistrictRegister.AddAsync(register);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateDistrictRegister(DistrictRegisterModel register)
+        {
+            //ArgumentNullException.ThrowIfNull(register);
+            var regiterToUpdate = await _dbContext.DistrictRegister.FirstOrDefaultAsync(r => r.Id == register.Id);
+            ArgumentNullException.ThrowIfNull(regiterToUpdate);
+            regiterToUpdate.YearInfo = register.YearInfo;
+            regiterToUpdate.QuarterInfo = register.QuarterInfo;
+            regiterToUpdate.EntryDate = register.EntryDate;
+
+            _dbContext.Update(regiterToUpdate);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteDistrictRegister(int id)
+        {
+            var regiterToDelete = await _dbContext.DistrictRegister.FirstOrDefaultAsync(r => r.Id == id);
+            ArgumentNullException.ThrowIfNull(regiterToDelete);
+            _dbContext.DistrictRegister.Remove(regiterToDelete);
+            await _dbContext.SaveChangesAsync();
+        }
+
+    }
+}
---
+++ WorkGroupProsecutor/Server/Data/Repositories/IDistrictRegisterRepository.cs
@@ -0,0 +1,13 @@
+﻿using WorkGroupProsecutor.Shared.Models;
+
+namespace WorkGroupProsecutor.Server.Data.Repositories
+{
+    public interface IDistrictRegisterRepository
+    {
+        Task<IList<DistrictRegisterModel>> GetAllDistrictRegistry();
+        Task<DistrictRegisterModel> GetDistrictRegisterById(int id);
+        Task AddDistrictRegister(DistrictRegisterModel register);
+        Task UpdateDistrictRegister(DistrictRegisterModel register); //Guid id, 
+        Task DeleteDistrictRegister(int id);
+    }
+}
