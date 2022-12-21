44b49a49751e34096229d1c953e33d0780a0c63a
Rh Proj <r.proj@hotmail.com>
2022-12-21 17:47:39 +0300

    UpsertSatisfiedAppeal

---
+++ WorkGroupProsecutor/Shared/Models/RecordDetails/AppealConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkGroupProsecutor.Shared.Models.RecordDetails
+{
+    public static class AppealConstants
+    {
+        public const int hyperLinkLength = 22;
+    }
+}
