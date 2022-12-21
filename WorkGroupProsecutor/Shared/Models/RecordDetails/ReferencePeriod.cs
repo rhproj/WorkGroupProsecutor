1f8e8bc6345554cdacccf0c275543aa8b7f9b51e
Rh Proj <r.proj@hotmail.com>
2022-12-21 14:17:39 +0300

    Satisfiedappealregistry page

---
+++ WorkGroupProsecutor/Shared/Models/RecordDetails/ReferencePeriod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkGroupProsecutor.Shared.Models.RecordDetails
+{
+    public static class ReferencePeriod
+    {
+        //public static readonly string[] QuartersList = { "I - квартал", "II - квартал", "III - квартал", "IV - квартал" };
+        public static readonly Dictionary<string, string> Quarters = new()
+        {
+            { "1", "I - квартал" },
+            { "2", "II - квартал"},
+            { "3", "III - квартал"},
+            { "4", "IV - квартал"}
+        };
+    
+    }
+}
