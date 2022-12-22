4586499b4cee5e24607dda38b9507cdcd0c10666
Rh Proj <r.proj@hotmail.com>
2022-12-22 17:43:40 +0300

    NoSolutionAppealController

--- WorkGroupProsecutor/Client/Services/NoSolutionAppealDataService.cs
+++ WorkGroupProsecutor/Client/Services/NoSolutionAppealDataService.cs
@@ -1,7 +1,65 @@
-﻿namespace WorkGroupProsecutor.Client.Services
-{
-    public class NoSolutionAppealDataService //: INoSolutionAppealDataService
-    {
+﻿using System.Text.Json;
+using System.Text;
+using WorkGroupProsecutor.Shared.Models.Appeal.DTO;
 
+namespace WorkGroupProsecutor.Client.Services
+{
+    public class NoSolutionAppealDataService : INoSolutionAppealDataService
+    {
+        private readonly HttpClient _httpClient;
+        public NoSolutionAppealDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<NoSolutionAppealModelDTO>> GetAllNoSolutionAppeals(string district, string period, int year)
+        {
+            return await JsonSerializer.DeserializeAsync<IEnumerable<NoSolutionAppealModelDTO>>
+                (await _httpClient.GetStreamAsync($"api/NoSolutionAppeal/{district}/{period}/{year}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+        }
+
+        public async Task<IEnumerable<NoSolutionAppealModelDTO>> GetAllNoSolutionAppealsForDepartment(string district, string department, string period, int year)
+        {
+            return await JsonSerializer.DeserializeAsync<IEnumerable<NoSolutionAppealModelDTO>>
+                (await _httpClient.GetStreamAsync($"api/NoSolutionAppeal/getAllForDepartment/{district}/{department}/{period}/{year}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+
+        public async Task<IEnumerable<string>> GetNoSolutionAppealsByDistricts(string period, int year)
+        {
+            return await JsonSerializer.DeserializeAsync<IEnumerable<string>>
+                (await _httpClient.GetStreamAsync($"api/NoSolutionAppeal/getByDistricts/{period}/{year}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+        }
+
+        public async Task<IEnumerable<string>> GetNoSolutionAppealsByDistrictsForDepartment(string department, string period, int year)
+        {
+            return await JsonSerializer.DeserializeAsync<IEnumerable<string>>
+                (await _httpClient.GetStreamAsync($"api/NoSolutionAppeal/getByDistrictsForDepartment/{department}/{period}/{year}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+        }
+
+        public async Task<NoSolutionAppealModelDTO> GetNoSolutionAppealById(int id)
+        {
+            return await JsonSerializer.DeserializeAsync<NoSolutionAppealModelDTO>
+                (await _httpClient.GetStreamAsync($"api/NoSolutionAppeal/{id}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+
+        public async Task AddNoSolutionAppeal(NoSolutionAppealModelDTO appeal)
+        {
+            var appealJson = new StringContent(JsonSerializer.Serialize(appeal), Encoding.UTF8, "application/json");
+            await _httpClient.PostAsync("api/NoSolutionAppeal", appealJson);
+        }
+
+        public async Task UpdateNoSolutionAppeal(NoSolutionAppealModelDTO appeal)
+        {
+            var appealJson = new StringContent(JsonSerializer.Serialize(appeal), Encoding.UTF8, "application/json");
+            await _httpClient.PutAsync("api/NoSolutionAppeal", appealJson);
+        }
+
+        public async Task DeleteNoSolutionAppeal(int id)
+        {
+            await _httpClient.DeleteAsync($"api/NoSolutionAppeal/{id}");
+        }
     }
 }
---
+++ WorkGroupProsecutor/Server/Controllers/NoSolutionAppealController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkGroupProsecutor.Server.Data.Repositories;
+using WorkGroupProsecutor.Shared.Models.Appeal.DTO;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WorkGroupProsecutor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoSolutionAppealController : ControllerBase
+    {
+        private readonly INoSolutionAppealRepository _appealRepository;
+        public NoSolutionAppealController(INoSolutionAppealRepository appealRepository)
+        {
+            _appealRepository = appealRepository;
+        }
+
+        /// <summary>
+        /// Возвращает все обращения района за отч.период, предполагаемые к переадресации в иные органы
+        /// </summary>
+        /// <param name="district">Район</param>
+        /// <param name="period">Отчетный период</param>
+        /// <param name="year">Отчетный год</param>
+        [HttpGet("{district}/{period}/{year}")]
+        public async Task<IActionResult> Get(string district, string period, int year)
+        {
+            return Ok(await _appealRepository.GetAllNoSolutionAppeals(district, period, year));
+        }
+
+        /// <summary>
+        /// Возвращает все обращения района за отч.период, для определенного отдела предполагаемые к переадресации в иные органы
+        /// </summary>
+        /// <param name="district">Район</param>
+        /// <param name="district">Отдел</param>
+        /// <param name="period">Отчетный период</param>
+        /// <param name="year">Отчетный год</param>
+        [HttpGet("getAllForDepartment/{district}/{department}/{period}/{year}")]
+        public async Task<IActionResult> GetAllForDepartment(string district, string department, string period, int year)  //m
+        {
+            return Ok(await _appealRepository.GetAllNoSolutionAppealsForDepartment(district, department, period, year));
+        }
+
+        #region for Dep-ts and w/o auth-n
+        /// <summary>
+        /// Cписок районнов имеющих обращения в указанный период
+        /// </summary>
+        /// <param name="period">Отчетный период</param>
+        /// <param name="year">Отчетный год</param>
+        /// <returns></returns>
+        [HttpGet("getByDistricts/{period}/{year}")]
+        public async Task<IActionResult> GetByDistricts(string period, int year)
+        {
+            return Ok(await _appealRepository.GetNoSolutiondAppealsByDistricts(period, year));
+        }
+
+        /// <summary>
+        /// Cписок районнов имеющих обращения в указанный период отфильтрованных для данного отдела
+        /// </summary>
+        /// <param name="department">Отдел</param>
+        /// <param name="period">Отчетный период</param>
+        /// <param name="year">Отчетный год</param>
+        /// <returns></returns>
+        [HttpGet("getByDistrictsForDepartment/{department}/{period}/{year}")]
+        public async Task<IActionResult> GetByDistrictsForDepartment(string department, string period, int year) //n
+        {
+            return Ok(await _appealRepository.GetNoSolutionAppealsByDistrictsForDepartment(department, period, year));
+        }
+        #endregion
+
+        /// <summary>
+        /// Возвращает обращение по заданному id
+        /// </summary>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _appealRepository.GetNoSolutionAppealById(id));
+        }
+
+        /// <summary>
+        /// Cоздает новое обращение
+        /// </summary>
+        /// <param name="appealDto">Модель обращения</param>
+        [HttpPost]
+        public async Task<IActionResult> Post(NoSolutionAppealModelDTO appealDto)
+        {
+            await _appealRepository.AddNoSolutionAppeal(appealDto);
+            return Ok("Обращение добавлено");
+        }
+
+        /// <summary>
+        /// Обновляет обращение
+        /// </summary>
+        /// <param name="appealDto">Модель обращения</param>
+        [HttpPut]   //("{id}")]
+        public async Task<IActionResult> Put(NoSolutionAppealModelDTO appealDto)
+        {
+            await _appealRepository.UpdateNoSolutionAppeal(appealDto);
+            return Ok("Обращение обнавлено");
+        }
+
+        /// <summary>
+        /// Удаляет обращение по заданному id
+        /// </summary>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _appealRepository.DeleteNoSolutionAppeal(id);
+            return Ok("Обращение удалено");
+        }
+    }
+}
