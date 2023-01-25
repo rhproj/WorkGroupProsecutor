a7ab826be3a326f8c338c23ce9cfb475affe6d74
Rh Proj <r.proj@hotmail.com>
2023-01-25 16:06:07 +0300

    CustomErrorBoundary

---
+++ WorkGroupProsecutor/Client/Shared/CustomErrorBoundary.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components;
+
+namespace WorkGroupProsecutor.Client.Shared
+{
+    public class CustomErrorBoundary : ErrorBoundary
+    {
+        [Inject]
+        private IWebAssemblyHostEnvironment Environment { get; set; }
+
+        protected override Task OnErrorAsync(Exception exception)
+        {
+            if (Environment.IsDevelopment())
+                return base.OnErrorAsync(exception);
+            else
+                return Task.CompletedTask;
+        }
+    }
+}
