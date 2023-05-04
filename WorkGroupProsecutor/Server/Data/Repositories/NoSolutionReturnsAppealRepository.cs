c19fa34f842f997ea3c34f32f26851e8538f3edf
Rh Proj <r.proj@hotmail.com>
2023-05-04 20:51:22 +0300

    NoSolutionReturnsAppealRepository methods

--- WorkGroupProsecutor/Server/Data/Repositories/NoSolutionReturnsAppealRepository.cs
+++ WorkGroupProsecutor/Server/Data/Repositories/NoSolutionReturnsAppealRepository.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using WorkGroupProsecutor.Server.Data.Context;
+using WorkGroupProsecutor.Shared.Models.Appeal;
 using WorkGroupProsecutor.Shared.Models.Appeal.DTO;
 
 namespace WorkGroupProsecutor.Server.Data.Repositories
@@ -36,57 +37,114 @@ namespace WorkGroupProsecutor.Server.Data.Repositories
         }
         #endregion
 
-        public Task AddNoSolutionReturnsAppeal(NoSolutionReturnsAppealModelDTO appeal)
+
+        public async Task<IEnumerable<NoSolutionReturnsAppealModelDTO>> GetAllNoSolutionReturnsAppeals(string district, string period, int year)
         {
-            throw new NotImplementedException();
+            var appeals = await _dbContext.NoSolutionAppeal.Include(a => a.Department)
+                .Where(a => a.District == district)
+                .Where(a => a.PeriodInfo == period)
+                .Where(a => a.YearInfo == year).ToListAsync();
+
+            return _mapper.Map<IEnumerable<NoSolutionReturnsAppealModelDTO>>(appeals);
         }
 
-        public Task DeleteNoSolutionReturnsAppeal(int id)
+        public async Task<IEnumerable<NoSolutionReturnsAppealModelDTO>> GetAllNoSolutionReturnsAppealsByDepartment(string district, string department, string period, int year)
         {
-            throw new NotImplementedException();
+            var appeals = await _dbContext.NoSolutionAppeal.Include(a => a.Department)
+                .Where(a => a.Department.DepartmentIndex == department)
+                .Where(a => a.District == district)
+                .Where(a => a.PeriodInfo == period)
+                .Where(a => a.YearInfo == year).ToListAsync();
+
+            return _mapper.Map<IEnumerable<NoSolutionReturnsAppealModelDTO>>(appeals);
         }
 
-        public Task<IEnumerable<NoSolutionReturnsAppealModelDTO>> GetAllNoSolutionReturnsAppeals(string district, string period, int year)
+        public async Task<IEnumerable<string>> GetNoSolutionReturnsAppealsByDistricts(string period, int year)
         {
-            throw new NotImplementedException();
+            return await _dbContext.NoSolutionAppeal
+                .Where(a => a.PeriodInfo == period)
+                .Where(a => a.YearInfo == year).Select(a => a.District).Distinct().ToListAsync();
         }
 
-        public Task<IEnumerable<NoSolutionReturnsAppealModelDTO>> GetAllNoSolutionReturnsAppealsByDepartment(string district, string department, string period, int year)
+        public async Task<IEnumerable<string>> GetNoSolutionReturnsAppelsByDistrictsForDepartment(string department, string period, int year)
         {
-            throw new NotImplementedException();
+            return await _dbContext.NoSolutionAppeal
+                .Where(a => a.Department.DepartmentIndex == department)
+                .Where(a => a.PeriodInfo == period)
+                .Where(a => a.YearInfo == year).Select(a => a.District).Distinct().ToListAsync();
         }
 
-
-        public Task<IEnumerable<NoSolutionReturnsAppealModelDTO>> GetAllNoSolutionReturnsUnansweredForDepartment(string department, string period, int year)
+        #region UNANSWERED
+        public async Task<IEnumerable<NoSolutionReturnsAppealModelDTO>> GetAllNoSolutionReturnsUnansweredForDepartment(string department, string period, int year)
         {
-            throw new NotImplementedException();
+            var appeals = await _dbContext.NoSolutionAppeal.Include(a => a.Department)
+                .Where(a => a.Department.DepartmentIndex == department)
+                .Where(a => a.PeriodInfo == period)
+                .Where(a => a.YearInfo == year)
+                .Where(a => a.DepartmentAssessment == null)
+                .ToListAsync();
+
+            return _mapper.Map<IEnumerable<NoSolutionReturnsAppealModelDTO>>(appeals);
         }
 
-        public Task<NoSolutionReturnsAppealModelDTO> GetNoSolutionReturnsAppealById(int id)
+        public async Task<int> GetUnansweredNumberForDepartment(string department, string period, int year)
         {
-            throw new NotImplementedException();
+            return await _dbContext.NoSolutionAppeal.Include(a => a.Department)
+                .Where(a => a.Department.DepartmentIndex == department)
+                .Where(a => a.PeriodInfo == period)
+                .Where(a => a.YearInfo == year)
+                .Where(a => a.DepartmentAssessment == null)
+                .CountAsync();
+        }
+        #endregion
+
+        public async Task<NoSolutionReturnsAppealModelDTO> GetNoSolutionReturnsAppealById(int id)
+        {
+            var appeal = await _dbContext.NoSolutionAppeal.FirstOrDefaultAsync(a => a.Id == id);
+            return _mapper.Map<NoSolutionReturnsAppealModelDTO>(appeal);
         }
 
-        public Task<IEnumerable<string>> GetNoSolutionReturnsAppealsByDistricts(string period, int year)
+        public async Task AddNoSolutionReturnsAppeal(NoSolutionReturnsAppealModelDTO appealDto)
         {
-            throw new NotImplementedException();
+            var appeal = _mapper.Map<NoSolutionAppealModel>(appealDto);
+
+            await _dbContext.NoSolutionAppeal.AddAsync(appeal);
+            await _dbContext.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<string>> GetNoSolutionReturnsAppelsByDistrictsForDepartment(string department, string period, int year)
+        public async Task UpdateNoSolutionReturnsAppeal(NoSolutionReturnsAppealModelDTO appealDto)
         {
-            throw new NotImplementedException();
+            var appealToEdit = await _dbContext.NoSolutionAppeal.FirstOrDefaultAsync(d => d.Id == appealDto.Id);
+            if (appealToEdit != null)
+            {
+                appealToEdit.District = appealDto.District;
+                appealToEdit.PeriodInfo = appealDto.PeriodInfo;
+                appealToEdit.YearInfo = appealDto.YearInfo;
+
+                appealToEdit.RegistrationNumber = appealDto.RegistrationNumber;
+                appealToEdit.NadzorHyperlink = appealDto.NadzorHyperlink;
+
+                appealToEdit.ApplicantFullName = appealDto.ApplicantFullName;
+
+                appealToEdit.DepartmentId = appealDto.DepartmentId;
+                appealToEdit.DepartmentAssessment = appealDto.DepartmentAssessment;
+
+                appealToEdit.DepartmentResolution = appealDto.DepartmentResolution;
+                appealToEdit.DecisionBasis = appealDto.DecisionBasis;
+
+                _dbContext.NoSolutionAppeal.Update(appealToEdit);
+                await _dbContext.SaveChangesAsync();
+            }
         }
 
-
-
-        public Task<int> GetUnansweredNumberForDepartment(string department, string period, int year)
+        public async Task DeleteNoSolutionReturnsAppeal(int id)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateNoSolutionReturnsAppeal(NoSolutionReturnsAppealModelDTO appeal)
-        {
-            throw new NotImplementedException();
+            var appeal = await _dbContext.NoSolutionAppeal.FirstOrDefaultAsync(a => a.Id == id);
+            if (appeal != null)
+            {
+                _dbContext.NoSolutionAppeal.Remove(appeal);
+                await _dbContext.SaveChangesAsync();
+            }
         }
     }
 }
